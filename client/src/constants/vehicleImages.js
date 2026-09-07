83a5e48928e6c46eaabf98a15d16546a83954710
kashish <kashish.vashistha1@gmail.com>
2026-09-07 23:29:01 +0530

    changes the vehicles images

--- client/src/constants/vehicleImages.js
+++ client/src/constants/vehicleImages.js
@@ -1,13 +1,13 @@
 export const vehicleDetails = {
     MOTO: {
         name: "Moto",
-        image: "https://i.ibb.co/n776JLm/bike.png",
+        image: "https://png.pngtree.com/png-vector/20241204/ourlarge/pngtree-motorbike-cartoon-image-without-background-png-image_14593702.png",
         capacity: 1,
         description: "Affordable motorcycle rides"
     },
     AUTO: {
         name: "Auto",
-        image: "https://i.ibb.co/MjgV3pM/uberauto.png",
+        image: "https://cn-geo1.uber.com/image-proc/crop/resizecrop/udam/format=auto/width=0/height=0/srcb64=aHR0cHM6Ly90Yi1zdGF0aWMudWJlci5jb20vcHJvZC91ZGFtLWFzc2V0cy9lZGJjMWQ5ZS01OGVjLTQyODAtYWJjNS0yNDA2M2JlMGRkMTUucG5n",
         capacity: 3,
         description: "Pay directly to driver, cash/UPI only"
     },
@@ -19,13 +19,13 @@ export const vehicleDetails = {
     },
     PREMIER: {
         name: "Premier",
-        image: "https://i.ibb.co/YdtWqz9/uberblack.png",
+        image: "https://p7.hiclipart.com/preview/624/624/60/mahindra-mahindra-mahindra-thar-crde-car-sport-utility-vehicle-four-wheel-drive-car-thumbnail.jpg",
         capacity: 4,
         description: "Comfortable sedans, top-quality drivers"
     },
     UBER_XL: {
         name: "UberXL",
-        image: "https://i.ibb.co/0VJWfK5/uberxl.png",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPdlCP8m_Ym8RxZqtU5UVNzgdIHXi7AByMvuXnQpU4p1mMItaoY8vtypF9&s=10",
         capacity: 6,
         description: "Comfortable SUVs for up to 6 people"
     }
