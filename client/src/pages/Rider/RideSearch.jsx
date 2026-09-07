7eb8afa5f0a48119e4e1dc765762d03d88d7475a
kashish <kashish.vashistha1@gmail.com>
2026-09-07 22:35:37 +0530

    fix bug

--- client/src/pages/Rider/RideSearch.jsx
+++ client/src/pages/Rider/RideSearch.jsx
@@ -90,7 +90,6 @@ export default function RideSearch() {
     useEffect(() => {
         if (rideError) {
             const msg = rideError?.response?.data?.message || "No drivers available near your location";
-            isRideFetching(false);
             toast.warning(msg);
         }
     }, [rideError]);
